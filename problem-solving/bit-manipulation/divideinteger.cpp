1e38e5f9863d94a50174ee9fd6f993f7fe631b58
Tushar Rajpoot <tusharrajput604@gmail.com>
2021-02-10 16:34:02 +0000

    Divide two integers without using / operator

--- problem-solving/bit-manipulation/divideinteger.cpp
+++ problem-solving/bit-manipulation/divideinteger.cpp
@@ -1,15 +1,38 @@
-#include<iostream>
+#include<bits/stdc++.h> 
 using namespace std;
-int main(){
-    int a;
-    int b;
-    cin>>a>>b;
-    int result = 1;
-    while (a>b)
-    {
-        result++;
-        a = a - b;
-        
+int divide(long long dividend, long long divisor) {
+
+  // Calculate sign of divisor i.e.,
+  // sign will be negative only iff
+  // either one of them is negative
+  // otherwise it will be positive
+  int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
+
+  // remove sign of operands
+  dividend = abs(dividend);
+  divisor = abs(divisor);
+
+  // Initialize the quotient
+  long long quotient = 0, temp = 0;
+
+  // test down from the highest bit and
+  // accumulate the tentative value for
+  // valid bit
+  for (int i = 31; i >= 0; --i) {
+
+    if (temp + (divisor << i) <= dividend) {
+      temp += divisor << i;
+      quotient |= 1LL << i;
     }
-    cout<<result<<endl;
+  }
+  return sign * quotient;
+}
+int main() {
+  int A, B;
+  cin >> A >> B;
+  if (A == INT_MIN && B == -1) {
+
+    cout << INT_MAX;
+  }
+  cout << divide((long long)A, (long long)B);
 }
