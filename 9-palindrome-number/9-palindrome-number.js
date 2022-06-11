845ae5e8381c4951a2fb3b6a0358ddf5be52db2b
Tushar Rajpoot <tusharrajput604@gmail.com>
2022-06-11 13:16:29 +0530

    Time: 258 ms (47.13%), Space: 51.4 MB (37.07%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,10 +3,5 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    const xstr = x.toString().split("").reverse().join("")
-    if(xstr===x.toString()){
-        return true;
-    }
-    return false;
-    
+    return x.toString().split("").reverse().join("")===x.toString()
 };
