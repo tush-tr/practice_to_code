9e3a2d34018c2001fff1024e964e17e070081fb4
Tushar Rajpoot <tusharrajput604@gmail.com>
2022-06-19 23:16:36 +0530

    Time: 77 ms (62.34%), Space: 41.3 MB (99.58%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
@@ -3,19 +3,13 @@
  * @return {number}
  */
 var sumOfUnique = function(nums) {
-    const idx = {}
-nums.forEach((el)=>{
-    idx[el]=0;
-})
-let sum=0;
-nums.forEach((el)=>{
-    idx[el]++;
-})
-Object.keys(idx).forEach((el)=>{
-    if(idx[el]===1){
-        sum = sum+parseInt(el);
+    let sum=0;
+    nums.sort()
+    for(let i=0;i<nums.length;i++){
+    if(nums[i]===nums[i+1] || nums[i]===nums[i-1]){
+        continue;
     }
-})
+    sum+=nums[i]
+}
     return sum
-    
 };
