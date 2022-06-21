fec2f87f889d1ccbc0e8747463b2d048224036ec
Tushar Rajpoot <tusharrajput604@gmail.com>
2022-06-21 23:59:59 +0530

    Time: 134 ms (31.08%), Space: 52.1 MB (15.61%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,25 +3,11 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    const aux = new Array(prices.length).fill(0);
-  const n = prices.length;
-  for (let i = n - 1; i >= 0; i--) {
-    if (i == n - 1) {
-      aux[i] = prices[i];
-    }
-    if (prices[i] >= aux[i + 1]) {
-      aux[i] = prices[i];
-    }
-    if(prices[i]<aux[i+1]){
-        aux[i]=aux[i+1]
-    }
-  }
-  let mxProfit = 0;
-  prices.forEach((price, i) => {
-    if (aux[i] - price > mxProfit) {
-      mxProfit = aux[i] - price;
-    }
-  });
-  return mxProfit;
-    
+    let min = Number.MAX_SAFE_INTEGER;
+    let mxProfit = Number.MIN_SAFE_INTEGER;
+    prices.forEach((price)=>{
+        min = Math.min(min,price);
+        mxProfit = Math.max(mxProfit,price-min);
+    })
+    return mxProfit
 };
