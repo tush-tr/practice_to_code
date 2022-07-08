b1bab6edb51d965afd87706de6a0a741d7114fbd
Tushar Rajpoot <tusharrajput604@gmail.com>
2022-07-09 00:51:17 +0530

    Added solution - LeetHub

---
+++ Missing number in array - GFG/missing-number-in-array.js
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+//Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let arr = new Array(n-1);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n-1;i++)
+            arr[i] = input_ar1[i];
+        let obj = new Solution();
+        console.log(obj.MissingNumber(arr, n));
+        
+    }
+}// } Driver Code Ends// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} array
+ * @param {number} n
+ * @returns {number}
+*/
+
+class Solution{
+    MissingNumber(array,n){
+        //code here
+        const sum = array.reduce((a,b)=>a+b)
+        let s = 0;
+        for(let i=1;i<=n;i++){
+            s+=i;
+        }
+        return s-sum
+    }
+}
