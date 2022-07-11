ad03daec07de4936a3b8dc79bc86a88cbcda2d95
Tushar Rajpoot <tusharrajput604@gmail.com>
2022-07-12 02:12:05 +0530

    Time: 103 ms (69.59%), Space: 50.8 MB (18.43%) - LeetHub

---
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+const kadane = (nums)=>{
+    let currSum = 0;
+    let maxSum = Number.MIN_SAFE_INTEGER;
+    nums.forEach((num)=>{
+        currSum += num;
+        maxSum = Math.max(maxSum,currSum);
+        if(currSum<0){
+            currSum = 0;
+        }
+        
+    })
+    return maxSum;
+}
+var maxSubarraySumCircular = function(nums) {
+    // TODO: get the total sum of the original array
+    const totalSum = nums.reduce((a,b)=>(a+b));
+    //  TODO: get the max subarray sum of nonWrapping elements
+    const nonWrapSum = kadane(nums);
+    if(nonWrapSum<0){
+        return nonWrapSum;
+    }
+    // TODO: Get the sum of non-contributing elements
+    const reverseSignArray = nums.map((num)=>(-(num)));
+    // TODO:  Max Subarray sum = Total sum  of array - Sum of non-contributing elements
+    const wrapSum = totalSum + kadane(reverseSignArray);
+    return Math.max(nonWrapSum,wrapSum);
+};
