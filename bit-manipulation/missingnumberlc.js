5c165373af4174137820e2e0e2b9b56c040854ef
tush-tr <tusharrajput604@gmail.com>
2022-07-25 03:55:25 +0530

    missing number problem from Leetcode

---
+++ bit-manipulation/missingnumberlc.js
@@ -0,0 +1,19 @@
+var missingNumber = function(nums) {
+    let n = nums.length;
+    nums.sort();
+    console.log(nums,n)
+    let ans;
+    for(let i=0;i<=n;i++){
+        let flag = false;
+        nums.forEach((num)=>{
+            if(i==num){
+                flag = true;
+            }
+        })
+        if(!flag){
+            ans = i;
+        }
+    }
+    return ans;
+};
+console.log(missingNumber([3,0,1]))
