a454ab30d7eda929395df02694be72d4ef04da4b
Tushar Rajpoot <tusharrajput604@gmail.com>
2022-08-25 01:47:42 +0530

    Time: 725 ms (5.00%), Space: 48.4 MB (5.00%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -2,9 +2,25 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
+var fibNotOptimized = function(n) {
     if(n==0 || n==1){
         return n;
     }
     return fib(n-1)+fib(n-2);
+};
+var fib = function(n) {
+    let f = new Array(n+1).fill(-1);
+    if(n==0 || n==1){
+        f[n] = n;
+        return n;
+    }
+    else{
+        if(f[n-2]==-1){
+            f[n-2] = fib(n-2);
+        }
+        if(f[n-1]==-1){
+            f[n-1] = fib(n-1);
+        }
+        return f[n-1]+f[n-2];
+    }
 };
